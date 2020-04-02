2ef05e4a5ea2e65d71b4f23ccfa74aad55b9e372
p.ficzere <ficzerepeti@gmail.com>
2020-04-02 16:08:46 +0200

    Hopefully finally figured out how Vault client lib behaves and added VaultConnection.cs

---
+++ git2sourceGearVault/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using VaultClientIntegrationLib;
+
+namespace git2sourceGearVault
+{
+    
+    internal static class Program
+    {
+        private const string VaultServer = "10.1.10.41";
+        private const string RepoFolderPath = "$/Misc/Git2SourceGear";
+        private static readonly string TempWorkingFolder = Path.Combine(Path.GetTempPath(), "git2sourceGear_working_folder");
+
+        public static void Main()
+        {
+            try
+            {
+                using (var vaultConnection = new VaultConnection(VaultServer, RepoFolderPath, TempWorkingFolder))
+                {
+                    ServerOperations.GetInstance().ChangesetOutput += csic => Console.WriteLine($"[ChangesetOutput] Change set count: {csic.Count}. {string.Join(", ", csic)}\n");
+                
+                    vaultConnection.Get();
+                    Console.WriteLine();
+                
+                    Directory.CreateDirectory(TempWorkingFolder);
+
+                    vaultConnection.DetectModifiedAndDeletedFiles();
+                    vaultConnection.PrintChangeSet();
+                    Console.WriteLine();
+
+                    const string folderName = "another_folder";
+                    Directory.CreateDirectory(Path.Combine(TempWorkingFolder, folderName));
+
+                    CreateAndAddFile(vaultConnection, "just_a_folder", "another_file1.txt", "111111111111");
+                    CreateAndAddFile(vaultConnection, folderName, "another_file1.txt", "111111111111");
+                    CreateAndAddFile(vaultConnection, folderName, "another_file2.txt", "222222222222");
+
+                    vaultConnection.Commit();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e.ToString());
+            }
+        }
+
+        private static void CreateAndAddFile(VaultConnection vaultConnection, string folderName, string fileName, string content)
+        {
+            var fileDiskPath = Path.Combine(TempWorkingFolder, folderName, fileName);
+            File.WriteAllText(fileDiskPath, content);
+        
+            Console.WriteLine($"Adding {fileDiskPath}");
+            vaultConnection.AddFile($"{RepoFolderPath}/{folderName}", fileDiskPath);
+            vaultConnection.PrintChangeSet();
+            Console.WriteLine();
+            vaultConnection.PrintChangeSetWithoutDuplicates();
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
---
+++ git2sourceGearVault/VaultConnection.cs
@@ -0,0 +1,145 @@
+﻿﻿using System;
+ using System.Collections.Generic;
+ using System.Linq;
+ using VaultClientIntegrationLib;
+ using VaultClientOperationsLib;
+
+ namespace git2sourceGearVault
+{
+    public class VaultConnection : IDisposable
+    {
+        private readonly string _vaultServer;
+        private readonly string _repoFolder;
+        private readonly string _workingFolder;
+        
+        private readonly ChangeSetItemColl _changes = new ChangeSetItemColl();
+
+        public VaultConnection(string vaultServerIp, string repoFolder, string workingFolder)
+        {
+            _vaultServer = vaultServerIp;
+            _repoFolder = repoFolder;
+            _workingFolder = workingFolder;
+
+            LoginAndClearChangeSet();
+        }
+
+        public void Get()
+        {
+            var getOptions = new GetOptions
+            {
+                MakeWritable = MakeWritableType.MakeAllFilesWritable, Recursive = true, Merge = MergeType.OverwriteWorkingCopy, PerformDeletions = PerformDeletionsType.RemoveWorkingCopy
+            };
+            Console.WriteLine("Getting from SourceGear");
+            GetOperations.ProcessCommandGet(new []{_repoFolder}, getOptions);
+        }
+        
+        public void AddFile(string repoPath, params string[] diskPaths) => _changes.AddRange(ServerOperations.ProcessCommandAdd(repoPath, diskPaths));
+
+        public void DetectModifiedAndDeletedFiles()
+        {
+            var listFolderResult = ServerOperations.ProcessCommandListFolder(_repoFolder, true);
+            
+            var diskPaths = listFolderResult.Files.Cast<VaultClientFile>().Select(x => x.FullPath).ToList();
+            diskPaths.AddRange(listFolderResult.Folders.Cast<VaultClientFolder>().Select(x => x.FullPath));
+            
+            var statuses = ServerOperations.ProcessCommandStatus(diskPaths.ToArray());
+
+            var pathAndStatus = diskPaths.Zip(statuses, Tuple.Create);
+            foreach (var (path, status) in pathAndStatus)
+            {
+                switch (status)
+                {
+                    case WorkingFolderFileStatus.None:
+                    case WorkingFolderFileStatus.Edited:
+                        break;
+                    
+                    case WorkingFolderFileStatus.Missing:
+                        _changes.AddRange(ServerOperations.ProcessCommandDelete(new[] {path}));
+                        Console.WriteLine($"Deleted {path} as it was {status}");
+                        break;
+                    
+                    case WorkingFolderFileStatus.Renegade:
+                        ServerOperations.ProcessCommandCheckout(new[] {path}, false, false, new GetOptions());
+                        _changes.AddRange(ServerOperations.ProcessCommandListChangeSet(new[] {path}));
+                        Console.WriteLine($"Checked out {path} as it was {status}");
+                        break;
+                    
+                    case WorkingFolderFileStatus.Merged:
+                    case WorkingFolderFileStatus.NeedsMerge:
+                    case WorkingFolderFileStatus.Unknown:
+                    case WorkingFolderFileStatus.MoreRecent:
+                    case WorkingFolderFileStatus.Old:
+                        Console.WriteLine($"Unhandled {path} with {status}");
+                        break;
+                        
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+
+        public void Commit()
+        {
+            var changes = RemoveDuplicatesFromChangeSet(_changes);
+            ServerOperations.ProcessCommandCommit(changes, UnchangedHandler.Checkin, false, LocalCopyType.Leave, false);
+        }
+
+        public void Dispose()
+        {
+            ServerOperations.ProcessCommandUndoCheckout(new []{_repoFolder}, true, LocalCopyType.Leave);
+            ServerOperations.Logout();
+        }
+
+        public void PrintChangeSet() => Console.WriteLine($"[{nameof(PrintChangeSet)}] Change set item count: {_changes.Count}. {string.Join(", ", _changes.Cast<ChangeSetItem>().Select(csi => $"{csi.DisplayRepositoryPath}-{csi.Type}"))}");
+        public void PrintChangeSetWithoutDuplicates()
+        {
+            var changes = RemoveDuplicatesFromChangeSet(_changes);
+            Console.WriteLine($"[{nameof(PrintChangeSetWithoutDuplicates)}] Change set item count: {changes.Count}. {string.Join(", ", changes.Cast<ChangeSetItem>().Select(csi => $"{csi.DisplayRepositoryPath}-{csi.Type}"))}");
+        }
+
+        private void LoginAndClearChangeSet()
+        {
+            Console.WriteLine($"About to log in to {_vaultServer}");
+            ServerOperations.SetLoginOptions($"http://{_vaultServer}/VaultService", "TestGit2SourceGear", "123456", "Hardcastle Source", false);
+            ServerOperations.Login();
+            Console.WriteLine("Connected");
+                
+            ServerOperations.client.MakeBackups = false;
+            ServerOperations.client.AutoCommit = false;
+            ServerOperations.client.Verbose = true;
+                
+            Console.WriteLine($"Setting working folder to {_workingFolder}");
+            ServerOperations.SetWorkingFolder(_repoFolder, _workingFolder, true);
+
+            Console.WriteLine($"{nameof(ServerOperations.isConnected)}: {ServerOperations.isConnected()}");
+
+            var lcs = ServerOperations.ProcessCommandListChangeSet(new []{_repoFolder});
+            if (lcs.Count <= 0) return;
+            
+            Console.WriteLine($"Removing {lcs.Count} item(s) from change set");
+            foreach (var _ in lcs)
+            {
+                ServerOperations.ProcessCommandUndoChangeSetItem(0);
+            }
+        }
+
+        private static ChangeSetItemColl RemoveDuplicatesFromChangeSet(ChangeSetItemColl input)
+        {
+            var result = new ChangeSetItemColl();
+            var dict = new Dictionary<string, ChangeSetItemType>();
+            foreach (ChangeSetItem item in input)
+            {
+                if (dict.TryGetValue(item.DisplayRepositoryPath, out var status))
+                {
+                    if (status != item.Type) throw new Exception($"{item.DisplayRepositoryPath} has multiple statuses: {status} and {item.Type}");
+                    continue;
+                }
+
+                dict[item.DisplayRepositoryPath] = item.Type;
+                result.Add(item);
+            }
+
+            return result;
+        }
+    }
+}
