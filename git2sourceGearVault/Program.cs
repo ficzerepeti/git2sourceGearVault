e451a09ad247e6f8dbb0a626cf0ad62bfe25901a
p.ficzere <ficzerepeti@gmail.com>
2020-04-02 18:08:51 +0200

    Fixed handling deleted files and folders

--- git2sourceGearVault/Program.cs
+++ git2sourceGearVault/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using VaultClientIntegrationLib;
+using VaultClientOperationsLib;
 
 namespace git2sourceGearVault
 {
@@ -8,7 +9,7 @@ namespace git2sourceGearVault
     internal static class Program
     {
         private const string VaultServer = "10.1.10.41";
-        private const string RepoFolderPath = "$/Misc/Git2SourceGear";
+        private const string RepoFolderPath = "$/misc/Git2SourceGear";
         private static readonly string TempWorkingFolder = Path.Combine(Path.GetTempPath(), "git2sourceGear_working_folder");
 
         public static void Main()
@@ -17,45 +18,34 @@ namespace git2sourceGearVault
             {
                 using (var vaultConnection = new VaultConnection(VaultServer, RepoFolderPath, TempWorkingFolder))
                 {
-                    ServerOperations.GetInstance().ChangesetOutput += csic => Console.WriteLine($"[ChangesetOutput] Change set count: {csic.Count}. {string.Join(", ", csic)}\n");
-                
-                    vaultConnection.Get();
-                    Console.WriteLine();
-                
-                    Directory.CreateDirectory(TempWorkingFolder);
+                    try
+                    {
+                        vaultConnection.Get();
+                        Console.WriteLine();
 
-                    vaultConnection.DetectModifiedAndDeletedFiles();
-                    vaultConnection.PrintChangeSet();
-                    Console.WriteLine();
+                        vaultConnection.DetectModifiedAndDeletedFiles();
+                        vaultConnection.PrintChangeSet();
+                        Console.WriteLine();
+                    
+                        Directory.Delete(Path.Combine(TempWorkingFolder, "just_a_folder"), true);
+                        File.Delete(Path.Combine(TempWorkingFolder, "another_folder", "another_file1.txt"));
+                        vaultConnection.DetectModifiedAndDeletedFiles();
+                        vaultConnection.PrintChangeSet();
+                        Console.WriteLine();
 
-                    const string folderName = "another_folder";
-                    Directory.CreateDirectory(Path.Combine(TempWorkingFolder, folderName));
-
-                    CreateAndAddFile(vaultConnection, "just_a_folder", "another_file1.txt", "111111111111");
-                    CreateAndAddFile(vaultConnection, folderName, "another_file1.txt", "111111111111");
-                    CreateAndAddFile(vaultConnection, folderName, "another_file2.txt", "222222222222");
-
-                    vaultConnection.Commit();
+                        vaultConnection.Commit();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"Before dispose: {e}");
+                        throw;
+                    }
                 }
             }
             catch (Exception e)
             {
                 Console.Error.WriteLine(e.ToString());
             }
-        }
-
-        private static void CreateAndAddFile(VaultConnection vaultConnection, string folderName, string fileName, string content)
-        {
-            var fileDiskPath = Path.Combine(TempWorkingFolder, folderName, fileName);
-            File.WriteAllText(fileDiskPath, content);
-        
-            Console.WriteLine($"Adding {fileDiskPath}");
-            vaultConnection.AddFile($"{RepoFolderPath}/{folderName}", fileDiskPath);
-            vaultConnection.PrintChangeSet();
-            Console.WriteLine();
-            vaultConnection.PrintChangeSetWithoutDuplicates();
-            Console.WriteLine();
-            Console.WriteLine();
         }
     }
 }
--- git2sourceGearVault/VaultConnection.cs
+++ git2sourceGearVault/VaultConnection.cs
@@ -1,10 +1,11 @@
 ﻿﻿using System;
- using System.Collections.Generic;
+using System.Collections.Generic;
+ using System.IO;
  using System.Linq;
- using VaultClientIntegrationLib;
- using VaultClientOperationsLib;
+using VaultClientIntegrationLib;
+using VaultClientOperationsLib;
 
- namespace git2sourceGearVault
+namespace git2sourceGearVault
 {
     public class VaultConnection : IDisposable
     {
@@ -13,6 +14,7 @@
         private readonly string _workingFolder;
         
         private readonly ChangeSetItemColl _changes = new ChangeSetItemColl();
+        private readonly HashSet<string> _repoDirsToDelete = new HashSet<string>();
 
         public VaultConnection(string vaultServerIp, string repoFolder, string workingFolder)
         {
@@ -20,7 +22,8 @@
             _repoFolder = repoFolder;
             _workingFolder = workingFolder;
 
-            LoginAndClearChangeSet();
+            Login();
+            ClearChangeSet();
         }
 
         public void Get()
@@ -37,11 +40,86 @@
 
         public void DetectModifiedAndDeletedFiles()
         {
-            var listFolderResult = ServerOperations.ProcessCommandListFolder(_repoFolder, true);
-            
-            var diskPaths = listFolderResult.Files.Cast<VaultClientFile>().Select(x => x.FullPath).ToList();
-            diskPaths.AddRange(listFolderResult.Folders.Cast<VaultClientFolder>().Select(x => x.FullPath));
-            
+            _repoDirsToDelete.Clear();
+            var folder = ServerOperations.ProcessCommandListFolder(_repoFolder, true);
+            HandleFolderChangesRecursively(folder);
+        }
+
+        public void Commit()
+        {
+            var changes = RemoveDuplicatesFromChangeSet(_changes);
+            ServerOperations.ProcessCommandCommit(changes, UnchangedHandler.Checkin, false, LocalCopyType.Leave, false);
+            _changes.Clear();
+        }
+
+        public void Dispose()
+        {
+            ServerOperations.ProcessCommandUndoCheckout(new []{_repoFolder}, true, LocalCopyType.Leave);
+            ClearChangeSet();
+            ServerOperations.RemoveWorkingFolder(_repoFolder);
+            ServerOperations.Logout();
+        }
+
+        public void PrintChangeSet() => Console.WriteLine($"[{nameof(PrintChangeSet)}] Change set item count: {_changes.Count}. {string.Join(", ", _changes.Cast<ChangeSetItem>().Select(csi => $"{csi.DisplayRepositoryPath}-{csi.Type}"))}");
+        public void PrintChangeSetWithoutDuplicates()
+        {
+            var changes = RemoveDuplicatesFromChangeSet(_changes);
+            Console.WriteLine($"[{nameof(PrintChangeSetWithoutDuplicates)}] Change set item count: {changes.Count}. {string.Join(", ", changes.Cast<ChangeSetItem>().Select(csi => $"{csi.DisplayRepositoryPath}-{csi.Type}"))}");
+        }
+
+        private void Login()
+        {
+            Console.WriteLine($"About to log in to {_vaultServer}");
+            ServerOperations.SetLoginOptions($"http://{_vaultServer}/VaultService", "TestGit2SourceGear", "123456", "Hardcastle Source", false);
+            ServerOperations.Login();
+            Console.WriteLine("Connected");
+                
+            ServerOperations.client.MakeBackups = false;
+            ServerOperations.client.AutoCommit = false;
+            ServerOperations.client.Verbose = true;
+                
+            Console.WriteLine($"Setting working folder to {_workingFolder}");
+            ServerOperations.RemoveWorkingFolder(_repoFolder);
+            ServerOperations.SetWorkingFolder(_repoFolder, _workingFolder, true);
+
+            Console.WriteLine($"{nameof(ServerOperations.isConnected)}: {ServerOperations.isConnected()}");
+        }
+
+        private void ClearChangeSet()
+        {
+            var lcs = ServerOperations.ProcessCommandListChangeSet(new[] {_repoFolder});
+            if (lcs.Count <= 0) return;
+
+            Console.WriteLine($"Removing {lcs.Count} item(s) from change set");
+            foreach (var _ in lcs)
+            {
+                ServerOperations.ProcessCommandUndoChangeSetItem(0);
+            }
+        }
+        
+        private void HandleFolderChangesRecursively(VaultClientFolder folder)
+        {
+            HandleFiles(folder.Files);
+            foreach (VaultClientFolder subFolder in folder.Folders)
+            {
+                HandleFolderChangesRecursively(subFolder);
+            }
+
+            if (string.Equals(_repoFolder, folder.FullPath, StringComparison.InvariantCultureIgnoreCase))
+            {
+                return;
+            }
+
+            var diskPath = MakeDiskPath(folder.FullPath);
+            if (!Directory.Exists(diskPath))
+            {
+                _repoDirsToDelete.Add(folder.FullPath);
+            }
+        }
+
+        private void HandleFiles(VaultClientFileColl fileColl)
+        {
+            var diskPaths = fileColl.Cast<VaultClientFile>().Select(x => x.FullPath).ToList();
             var statuses = ServerOperations.ProcessCommandStatus(diskPaths.ToArray());
 
             var pathAndStatus = diskPaths.Zip(statuses, Tuple.Create);
@@ -78,52 +156,7 @@
             }
         }
 
-        public void Commit()
-        {
-            var changes = RemoveDuplicatesFromChangeSet(_changes);
-            ServerOperations.ProcessCommandCommit(changes, UnchangedHandler.Checkin, false, LocalCopyType.Leave, false);
-        }
-
-        public void Dispose()
-        {
-            ServerOperations.ProcessCommandUndoCheckout(new []{_repoFolder}, true, LocalCopyType.Leave);
-            ServerOperations.Logout();
-        }
-
-        public void PrintChangeSet() => Console.WriteLine($"[{nameof(PrintChangeSet)}] Change set item count: {_changes.Count}. {string.Join(", ", _changes.Cast<ChangeSetItem>().Select(csi => $"{csi.DisplayRepositoryPath}-{csi.Type}"))}");
-        public void PrintChangeSetWithoutDuplicates()
-        {
-            var changes = RemoveDuplicatesFromChangeSet(_changes);
-            Console.WriteLine($"[{nameof(PrintChangeSetWithoutDuplicates)}] Change set item count: {changes.Count}. {string.Join(", ", changes.Cast<ChangeSetItem>().Select(csi => $"{csi.DisplayRepositoryPath}-{csi.Type}"))}");
-        }
-
-        private void LoginAndClearChangeSet()
-        {
-            Console.WriteLine($"About to log in to {_vaultServer}");
-            ServerOperations.SetLoginOptions($"http://{_vaultServer}/VaultService", "TestGit2SourceGear", "123456", "Hardcastle Source", false);
-            ServerOperations.Login();
-            Console.WriteLine("Connected");
-                
-            ServerOperations.client.MakeBackups = false;
-            ServerOperations.client.AutoCommit = false;
-            ServerOperations.client.Verbose = true;
-                
-            Console.WriteLine($"Setting working folder to {_workingFolder}");
-            ServerOperations.SetWorkingFolder(_repoFolder, _workingFolder, true);
-
-            Console.WriteLine($"{nameof(ServerOperations.isConnected)}: {ServerOperations.isConnected()}");
-
-            var lcs = ServerOperations.ProcessCommandListChangeSet(new []{_repoFolder});
-            if (lcs.Count <= 0) return;
-            
-            Console.WriteLine($"Removing {lcs.Count} item(s) from change set");
-            foreach (var _ in lcs)
-            {
-                ServerOperations.ProcessCommandUndoChangeSetItem(0);
-            }
-        }
-
-        private static ChangeSetItemColl RemoveDuplicatesFromChangeSet(ChangeSetItemColl input)
+        private ChangeSetItemColl RemoveDuplicatesFromChangeSet(ChangeSetItemColl input)
         {
             var result = new ChangeSetItemColl();
             var dict = new Dictionary<string, ChangeSetItemType>();
@@ -135,11 +168,26 @@
                     continue;
                 }
 
+                if (_repoDirsToDelete.Any(x => item.DisplayRepositoryPath.StartsWith(x, StringComparison.OrdinalIgnoreCase)))
+                {
+                    Console.WriteLine($"Removing {item.DisplayRepositoryPath} from change set as it is in a deleted folder");
+                    continue;
+                }
+
                 dict[item.DisplayRepositoryPath] = item.Type;
                 result.Add(item);
             }
 
+            result.AddRange(ServerOperations.ProcessCommandDelete(_repoDirsToDelete.ToArray()));
+
             return result;
+        }
+
+        private string MakeDiskPath(string repoPath)
+        {
+            if (!repoPath.StartsWith(_repoFolder, StringComparison.InvariantCultureIgnoreCase)) throw new Exception($"Path {repoPath} doesn't start with {_repoFolder}");
+            var suffix = repoPath.Substring(_repoFolder.Length);
+            return _workingFolder + suffix;
         }
     }
 }
